8f40e6e2e33eb3df7e73b24e7e70d47440bd45f6
Luis Rafael Deluvio <deluvioluisrafael1@gmail.com>
2024-04-29 07:20:32 +0800

    Finished expenses structure card.

--- site/script.js
+++ site/script.js
@@ -1,24 +1,42 @@
-const totalCash = 20;
+let totalCash = 20.16;
+const currency = "PHP";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     x = document.getElementsByClassName("total-cash");
     for (var i = 0; i < x.length; i++) {
-        x[i].innerText = totalCash;
+        x[i].innerText = currency + " " + totalCash;
     };
 });
-
 
 var chrt = document.getElementById("donut-expense").getContext("2d");
       var chartId = new Chart(chrt, {
          type: 'doughnut',
          data: {
-            labels: ["HTML", "CSS", "JAVASCRIPT", "CHART.JS", "JQUERY", "BOOTSTRP"],
+            labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
             datasets: [{
                label: "Online Tutorial Subjects",
-               data: [20, 40, 30, 35, 30, 20],
+               data: [20, 40, 30, 35, 30],
             }],
          },
          options: {
             responsive: false,
+            animation: false,
+            cutout: '65%',
+            plugins: {
+               legend: {
+                   position: 'bottom', //set the legeds to the bottom
+                   align: 'start',
+                labels: {
+                    usePointStyle: true,
+                    boxWidth: 7,
+                    boxHeight: 7,
+                }
+               },
+            },
+            elements: {
+                arc: {
+                    borderWidth: 0 //remove the barriers between each data
+                },
+            },
          },
       });
